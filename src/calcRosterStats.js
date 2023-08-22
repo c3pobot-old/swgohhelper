46d61742f68fb3d4112f19cfb82a347098b142c9
scuba <scuba75@gmail.com>
2023-08-22 15:20:17 +0000

    update

--- src/calcRosterStats.js
+++ src/calcRosterStats.js
@@ -1,11 +1,11 @@
 'use strict'
 const statCalc = require('statcalc')
-module.exports = async(rosterUnit = [], allyCode, calcExtraStats = false)=>{
+module.exports = (rosterUnit = [], allyCode, calcExtraStats = false)=>{
   try{
     if(!rosterUnit || rosterUnit.length === 0) throw(`${allyCode} has no units`)
-    let summary = statCalc.calcRosterStats(rosterUnit)
+    let profile = statCalc.calcRosterStats(rosterUnit)
     if(profile.summary){
-      let res { zetaCount: profile.summary.zetaCount, sixModCount: profile.summary.zetaCount, omiCount: profile.summary.omi, roster: profile.roster, summary: profile.summary}
+      let res = { zetaCount: profile.summary.zeta, sixModCount: profile.summary.mod.r6, omiCount: profile.summary.omi, roster: profile.roster, summary: profile.summary}
       res.omiCount.gac = profile.summary.omi.ga
       res.omiCount.conquest = profile.summary.omi.cq
       return res
