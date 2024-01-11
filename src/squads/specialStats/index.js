81f2814a4adbd74bc40c1f8dfb4b4ce014bed22c
scuba <scuba75@gmail.com>
2024-01-11 13:50:34 +0000

    update

--- src/squads/specialStats/index.js
+++ src/squads/specialStats/index.js
@@ -1,4 +1,5 @@
 'use strict'
 const Cmds = {}
 Cmds['2000'] = require('./healthRatio')
+Cmds['2001'] = require('./totalHp')
 module.exports = Cmds
---
+++ src/squads/specialStats/totalHp.js
@@ -0,0 +1,20 @@
+'use strict'
+module.exports = async(stats = {}, requirement = {}, res = {})=>{
+  try{
+    let protection = (stats.base['28'] || 0)
+    let health = (stats.base['1'] || 0)
+    if(stats.gear){
+      protection += (stats.gear['28'] || 0) + (stats.mods['28'] || 0)
+      health += (stats.gear['1'] || 0) + (stats.mods['1'] || 0)
+    }
+    if(stats.crew){
+      protection += (stats.crew['28'] || 0)
+      health += (stats.crew['1'] || 0)
+    }
+    res.stat = protection + health
+    if(res.stat < requirement.min) res.notMet = 1
+    if(requirement.max && res.stat > requirement.max) res.notMet = 1
+  }catch(e){
+    console.error(e)
+  }
+}
